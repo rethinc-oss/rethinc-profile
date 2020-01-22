2cf2a0614b58060307105db99d7097a63324997e
Tim Kettler <tim.kettler@rethinc.de>
2020-01-22 14:45:17 +0100

    Added documentation for the profile::server::base profile.

--- manifests/server/base.pp
+++ manifests/server/base.pp
@@ -1,9 +1,51 @@
-# A description of what this class does
+# This class manages the common base configuration for all servers. That is:
 #
-# @summary A short summary of the purpose of this class
+#   - it configures the core location and language settings of a server
+#     (system locale, keyboard layout, timezone).
+#   - It configures the apt update & upgrade policy.
+#   - It sets up time synchronization
+#   - It creates the central system-management user.
+#   - It install a few core system-management utilities.
+#
+# @summary Manages the common base configuration for all servers
 #
 # @example
 #   include profile::server::base
+#
+# @param timezone
+#   The timezone used for calculting the local time. The list of valid
+#   values can be printed with: timedatectl list-timezones. Default value:
+#   Europe/Berlin.
+# @param keyboard_layout
+#   The keyboard layout top use. Default value: de.
+# @param locale
+#   The system locale. Default value: en_US.UTF-8.
+# @param management_user_name
+#   The descriptive name of the system-management user. Default value: System Operator.
+# @param management_user_login
+#   The login name of the system-management user. Default value: sysop
+# @param management_user_password
+#   The password of the system-management user. Default value: n/a.
+# @param management_user_addon_groups
+#   An array of groups, the system-management user should be a member of in addition to
+#   the default group memberships assigned at user creation. By default there is one
+#   group specified, its value is looked up from the profile-level hiera variable
+#   profile::server::management_group. Default value [operator]
+# @param management_user_public_keys
+#   An array of unique values (usualy email addresses). The values are looked up as
+#   keys in the profile-level hiera variable profile::server::ssh::keys. The keys are
+#   added as authorized public ssh-keys:
+#
+#     profile::server::ssh::keys:
+#       alice@example.com:
+#         type: ssh-ed25519
+#         key: AAAAC3NzaC1lZdeF54E5AAAAIBKQkiccEOf3yww62zTQwZSPX96eL7zVqPmAF56lnW
+#         comment: Alice (Login Key)
+#       bob@example.com:
+#         type: ssh-ed25519
+#         key: AAAAC3NzaC1lZdeF54E5AAAAIBKQkiccEOf3yww62zTQwZSPX96eL7zVqPmAF56lnW
+#         comment: Alice (Login Key)
+#
 class profile::server::base (
   String $timezone,
   String $keyboard_layout,
