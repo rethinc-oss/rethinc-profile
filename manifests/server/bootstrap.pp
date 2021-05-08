79153ab1045f7b0b3657a0246a9c49c7ffa8b89a
Tim Kettler <tim.kettler@rethinc.de>
2021-05-08 11:59:11 +0200

    Properly disable unattended upgrades
    Disable the correct timer and wait for a possible running timer execution to finish.

--- manifests/server/bootstrap.pp
+++ manifests/server/bootstrap.pp
@@ -7,12 +7,22 @@
 class profile::server::bootstrap (
   Boolean $unattended_upgrades,
 ){
-  service { 'unattended-upgrades':
-    ensure   => $unattended_upgrades,
-    provider => 'systemd',
-    enable   => $unattended_upgrades,
+  if (!$unattended_upgrades)
+  {
+    systemd::dropin_file { 'apt-daily.timer.conf':
+      unit   => 'apt-daily.timer',
+      source => "puppet:///modules/${module_name}/apt/apt-daily.timer.conf",
+    }
+    ~> service {'apt-daily.timer':
+      ensure   => false,
+      enable   => false,
+    }
   }
-  -> exec { 'apt update':
+  -> exec { 'boostrap_wait_for_unattended_upgrades':
+    command => '/usr/bin/systemd-run --property="After=apt-daily.service apt-daily-upgrade.service" --wait /bin/true',
+    unless  => '/usr/bin/test -f /var/cache/apt/.intial_update_done',
+  }
+  -> exec { 'bootstrap_initial_apt_update':
     command => '/usr/bin/apt update && /usr/bin/apt install -y software-properties-common',
     unless  => '/usr/bin/test -f /var/cache/apt/.intial_update_done',
   }
